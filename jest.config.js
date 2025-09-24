a900280a13b90c1b25b7b66388081943523e2364
BraedenLyman <108896258+BraedenLyman@users.noreply.github.com>
2025-09-24 19:57:53 -0400

    updated jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["js", "jsx"],
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest"
+  },
+  moduleNameMapper: {
+    "\\.css$": "identity-obj-proxy"
+  },
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"]
+};
