f2bc39c6a58f73ccae3cbb8b6ab9ac7d72a6ba4c
BraedenLyman <108896258+BraedenLyman@users.noreply.github.com>
2025-09-27 19:30:29 -0400

    removed testing stuff for now

--- cypress/support/e2e.js
+++
@@ -1,8 +0,0 @@
-// Import commands.js
-import './commands'
-
-// Disable uncaught exception handling for cleaner test output
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from failing the test
-  return false
-})
