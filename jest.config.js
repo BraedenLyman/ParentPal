f27f763c5f057580c69020602dbe9b9765579d93
BraedenLyman <108896258+BraedenLyman@users.noreply.github.com>
2025-09-28 10:20:02 -0400

    Added automated testing setup

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+export default {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.(js|jsx|ts|tsx)',
+    '<rootDir>/src/**/?(*.)(spec|test).(js|jsx|ts|tsx)'
+  ],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/main.tsx',
+    '!src/vite-env.d.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  }
+};
