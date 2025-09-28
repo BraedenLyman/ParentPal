2732ef7c87e2475cb1c7b7b3da2b73e1e225ac89
BraedenLyman <108896258+BraedenLyman@users.noreply.github.com>
2025-09-28 11:13:49 -0400

    updates to babel and jest

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,10 @@ export default {
     '<rootDir>/src/**/__tests__/**/*.(js|jsx|ts|tsx)',
     '<rootDir>/src/**/?(*.)(spec|test).(js|jsx|ts|tsx)'
   ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/src/__tests__/.*utils.*'
+  ],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
