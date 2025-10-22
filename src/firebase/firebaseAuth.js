dfa93917037d9597036511bea1555de787815c0c
BraedenLyman <108896258+BraedenLyman@users.noreply.github.com>
2025-10-22 19:29:01 -0400

    Update firebaseAuth.js

--- src/firebase/firebaseAuth.js
+++ src/firebase/firebaseAuth.js
@@ -1,11 +1,11 @@
-import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
+import { getAuth, setPersistence, browserSessionPersistence } from "firebase/auth";
 import { app } from "./firebase";
 
 export const auth = getAuth(app);
 
-setPersistence(auth, browserLocalPersistence)
+setPersistence(auth, browserSessionPersistence)
   .then(() => {
-    console.log("Auth persistence set to LOCAL");
+    console.log("Auth persistence set to SESSION");
   })
   .catch((error) => {
     console.error("Error setting auth persistence:", error);
